1471a4e26352e27aede10cf30a54e39e4dc5a6de
jlrfocus <40567091+jlrfocus@users.noreply.github.com>
2020-11-15 00:10:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+var index = [
+	{
+		"title" : "Age Calculator",
+		"img" : "img/c5r.png",
+		"bg": "#00b7ff",
+		"type" : "function",
+		"link" : "JavaScript/age calculator.html",
+		"version" : "v1.0"
+	},{
+		"title" : "Python Compiler",
+		"img" : "img/d2.png",
+		"bg": "#00b7ff",
+		"type" : "compiler",
+		"link" : "JavaScript/python compiler/index1.html",
+		"version" : "Unknown"
+	},{
+		"title" : "TicTacToe",
+		"img" : "img/c8t.png",
+		"bg": "#00b7ff",
+		"type" : "game",
+		"link" : "JavaScript/tic tac toe/tic tac toe.html",
+		"version" : "v5.0"
+	},{
+		"title" : "Rock Paper Scissor",
+		"img" : "img/bk5.png",
+		"bg": "#00b7ff",
+		"type" : "game",
+		"link" : "JavaScript/rock paper scissor.html",
+		"version" : "v7.0"
+	},{
+		"title" : "Lap Timer",
+		"img" : "img/m-.png",
+		"bg": "#00b7ff",
+		"type" : "function",
+		"link" : "JavaScript/lap timer/v4.4.html",
+		"version" : "v4.4"
+	},{
+		"title" : "Roman Numerals",
+		"img" : "img/dp2.png",
+		"bg": "#00b7ff",
+		"type" : "converter",
+		"link" : "JavaScript/Roman Numerals/v4.0.html",
+		"version" : "v4.0"
+	},{
+		"title" : "Binary Converter",
+		"img" : "img/bj0.png",
+		"bg": "#00b7ff",
+		"type" : "converter",
+		"link" : "JavaScript/Binary Converter/v4.0.html",
+		"version" : "v4.0"
+	},{
+		"title" : "String Helper",
+		"img" : "img/bl8.png",
+		"bg": "#00b7ff",
+		"type" : "helper",
+		"link" : "JavaScript/String helper/v3.1/index.html",
+		"version" : "v3.1"
+	},{
+		"title" : "Hex Converter",
+		"img" : "img/bl8.png",
+		"bg": "#00b7ff",
+		"type" : "Converter",
+		"link" : "JavaScript/Hex Converter/v1.1.html",
+		"version" : "v1.1"
+	},{
+		"title" : "CSS Beautifier",
+		"img" : "img/bl8.png",
+		"bg": "#00b7ff",
+		"type" : "Helper",
+		"link" : "JavaScript/CSS Beautifier/v1.2_beta.html",
+		"version" : "Beta v1.2"
+	},{
+		"title" : "String to Elements",
+		"img" : "img/bl8.png",
+		"bg": "#00b7ff",
+		"type" : "Converter",
+		"link" : "JavaScript/String to Elements/v1.1 beta/index.html",
+		"version" : "Beta v1.1"
+	}
+	
+	
+];
+
+function getData(){
+	return index;
+}
